352548efe7079b42211c89ef7f34abea850f6b26
HugeDog <lacertidhk@gmail.com>
2022-07-11 13:36:46 +0800

    闲来无事

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # 我先顽皮一哈子
 
-build_dir="./build"
-
-#########################################
 red(){
     echo -e "\033[31m\033[01m$1\033[0m"
 }
@@ -19,38 +16,99 @@ blue(){
 bold(){
     echo -e "\033[1m\033[01m$1\033[0m"
 }
-##########################################
 
-blue "-----Jinqi Li and Chenchu Zhang Build Shell-----"
+function checkos(){
+    if [[ -f /etc/redhat-release ]]; then
+        osRelease="centos"
+        osSystemPackage="yum"
+        osSystemMdPath="/usr/lib/systemd/system/"
+        osReleaseVersionCodeName=""
+    elif cat /etc/issue | grep -Eqi "debian|raspbian"; then
+        osRelease="debian"
+        osSystemPackage="apt-get"
+        osSystemMdPath="/lib/systemd/system/"
+        osReleaseVersionCodeName="buster"
+    elif cat /etc/issue | grep -Eqi "ubuntu"; then
+        osRelease="ubuntu"
+        osSystemPackage="apt-get"
+        osSystemMdPath="/lib/systemd/system/"
+        osReleaseVersionCodeName="bionic"
+    elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
+        osRelease="centos"
+        osSystemPackage="yum"
+        osSystemMdPath="/usr/lib/systemd/system/"
+        osReleaseVersionCodeName=""
+    elif cat /proc/version | grep -Eqi "debian|raspbian"; then
+        osRelease="debian"
+        osSystemPackage="apt-get"
+        osSystemMdPath="/lib/systemd/system/"
+        osReleaseVersionCodeName="buster"
+    elif cat /proc/version | grep -Eqi "ubuntu"; then
+        osRelease="ubuntu"
+        osSystemPackage="apt-get"
+        osSystemMdPath="/lib/systemd/system/"
+        osReleaseVersionCodeName="bionic"
+    elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
+        osRelease="centos"
+        osSystemPackage="yum"
+        osSystemMdPath="/usr/lib/systemd/system/"
+        osReleaseVersionCodeName=""
+    fi
+    blue "Your System: $osRelease"
+    blue "Package: $osSystemPackage"
+}
 
-if [ -e $build_dir ]
-then 
-    green "Directory Exists!"
-    blue "Deleting..."
-    rm -rf $build_dir
-else
-    red "Directory NOT Exists!"
-fi
+function pkgs(){
+    if ! dpkg -l | grep -qw $1; then
+        red "$1 NOT Installed !"
+        read -p "Install? [Y/n]:" isContinueInput
+	    isContinueInput=${isContinueInput:-Y}
+        if [[ $isContinueInput == [Yy] ]]; then
+	 	    ${osSystemPackage} -y install $1
+	    else 
+	 	    red "Install Cancelled!"
+	    fi
+    else
+        green "$1 Installed !"
+	fi
+}
 
-blue "Make Dir..."
-mkdir $build_dir
-green "Make Dir Successfully !!"
+function builds(){
+    build_dir="./build"
+    if [ -e $build_dir ]
+    then 
+        green "Directory Exists!"
+        blue "Deleting..."
+        rm -rf $build_dir
+    else
+        red "Directory NOT Exists!"
+    fi
+    blue "Make Dir..."
+    mkdir $build_dir
+    green "Make Dir Successfully !!"
+    blue "================= BUILDING... ==================="
+    blue "Enter Directory..."
+    cd $build_dir
+    blue "============= CMAKE ==============="
+    cmake ..
+    blue "============= MAKE ==============="
+    make
+    chmod 777 ./*
+    green "Leave Directory..."
+    green "==================== ENDS! ======================"
+    green "Done!"
+    blue "-----Jinqi Li and Chenchu Zhang Build Shell-----"
+}
 
-blue "================= BUILDING... ==================="
-blue "Enter Directory..."
-
-cd $build_dir
-
-blue "============= CMAKE ==============="
-cmake ..
-
-blue "============= MAKE ==============="
-make
-
-green "Leave Directory..."
-green "==================== ENDS! ======================"
-green "Done!"
-green "-----Jinqi Li and Chenchu Zhang Build Shell-----"
-yellow "-----Jinqi Li and Chenchu Zhang Build Shell-----"
-red "-----Jinqi Li and Chenchu Zhang Build Shell-----"
-blue "-----Jinqi Li and Chenchu Zhang Build Shell-----"
+################################################################
+yellow "--------------------------------"
+checkos
+yellow "--------------------------------"
+yellow "================== Check Package ================="
+pkgs gcc
+pkgs g++
+pkgs cmake
+pkgs libssl-dev
+yellow "===================== Build ======================"
+builds
+################################################################
